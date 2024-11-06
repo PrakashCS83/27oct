b8cdd48c52dad2d01e592f1b3b813b12ad3bc7e4
Prakash <awsprakash2024@gmail.com>
2024-11-06 12:59:41 +0530

    ec2 modifeied

--- ec2.tf
+++ ec2.tf
@@ -1,33 +1,36 @@
 resource "aws_instance" "worker1" {
-    ami = "ami-022ce6f32988af5fa"
-    instance_type = "t2.micro"
-    key_name = "PrakashAWS"
-    subnet_id = aws_subnet.public-subnets["public-subnet2"].id
-    vpc_security_group_ids = [aws_security_group.Terra_SG.id]
-    associate_public_ip_address = true
-    tags = {
-      Name = "Ansible Worker1"
-    }
+  ami                    = "ami-022ce6f32988af5fa" # Replace with a valid AMI ID
+  instance_type          = "t2.micro"
+  key_name               = "PrakashAWS"
+  subnet_id              = aws_subnet.public-subnets["public-subnet2"].id
+  vpc_security_group_ids = [aws_security_group.Terra_SG.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Ansible Worker1"
+  }
+
   user_data = <<-EOF
 #!/bin/bash
 useradd abhi
 echo "redhat" | passwd --stdin abhi
 sed -i "/PasswordAuthentication/s/no/yes/" /etc/ssh/sshd_config
-sed -i "/PasswordAuthentication/s/no/yes/" /etc/ssh/sshd_config.d/50-cloud-init.conf
+sed -i "/PasswordAuthentication/s/no/yes/" /etc/ssh/sshd_config.d/50-cloud-init.conf || true
 systemctl restart sshd
 echo "abhi ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 EOF
 }
+
 # Output the instance's public IP
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+output "worker1_public_ip" {
+  value = aws_instance.worker1.public_ip
 }
 
 # Write the public IP to the Ansible inventory file on the Terraform/Ansible server
 resource "local_file" "ansible_inventory" {
   content = <<EOF
 [ec2_instances]
-${aws_instance.example.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=/var/tmp/private-key.pem
+${aws_instance.worker1.public_ip} ansible_user=abhi ansible_ssh_private_key_file=/var/tmp/private-key.pem
 EOF
   filename = "/var/tmp/ansible/inventory.ini"
 }
